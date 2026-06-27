3f4bc78c62ed3a66422c55b9ea9379844979bf1a
nobiamebet-byte <nobiamebet@gmail.com>
2026-06-27 13:26:16 +0500

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,47 +1,29 @@
 LOCAL_PATH := $(call my-dir)
 
-# ─── Prebuilt: Dobby ───────────────────────────────
 include $(CLEAR_VARS)
-LOCAL_MODULE    := dobby
+LOCAL_MODULE := dobby
 LOCAL_SRC_FILES := Dobby/libraries/$(TARGET_ARCH_ABI)/libdobby.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-# ─── Prebuilt: Keystone ─────────────────────────────
 include $(CLEAR_VARS)
-LOCAL_MODULE    := Keystone
+LOCAL_MODULE := Keystone
 LOCAL_SRC_FILES := KittyMemory/Deps/Keystone/libs-android/$(TARGET_ARCH_ABI)/libkeystone.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-# Here is the name of your lib.
-# When you change the lib name, change also on System.loadLibrary("") under OnCreate method on Main.java
-# Both must have same name
 include $(CLEAR_VARS)
-LOCAL_MODULE    := MyLibName
+LOCAL_MODULE := MyLibName
+LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/Includes/
 
-# -std=c++17 is required to support AIDE app with NDK
-LOCAL_CFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -fpermissive -fexceptions
-LOCAL_CPPFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -Werror -std=c++17
-LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fpermissive -Wall -fexceptions
-LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all,-llog
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
-LOCAL_ARM_MODE := arm
-
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/Includes/
-
-# Here you add the cpp file to compile
 LOCAL_SRC_FILES := Main.cpp \
-    Menu/Jni.cpp \
-    Menu/Menu.cpp \
-    Menu/Setup.cpp \
-    Includes/Utils.cpp \
-	KittyMemory/KittyArm64.cpp \
-    KittyMemory/KittyScanner.cpp \
-    KittyMemory/KittyMemory.cpp \
-    KittyMemory/KittyUtils.cpp \
-    KittyMemory/MemoryPatch.cpp \
-    KittyMemory/MemoryBackup.cpp
+                   Includes/Utils.cpp \
+                   KittyMemory/KittyArm64.cpp \
+                   KittyMemory/KittyScanner.cpp \
+                   KittyMemory/KittyMemory.cpp \
+                   KittyMemory/KittyUtils.cpp \
+                   KittyMemory/MemoryPatch.cpp \
+                   KittyMemory/MemoryBackup.cpp
 
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
 LOCAL_STATIC_LIBRARIES := Keystone dobby
-
 include $(BUILD_SHARED_LIBRARY)
+
